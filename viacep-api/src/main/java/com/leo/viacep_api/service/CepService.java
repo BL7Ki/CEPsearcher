9ca32dcb20a87be2ffc531e6a91e78ed1264144c
Leonardo Ferreira <leoventura245@gmail.com>
2025-02-12 18:58:39 -0300

    Building config

---
+++ viacep-api/src/main/java/com/leo/viacep_api/service/CepService.java
@@ -0,0 +1,12 @@
+package com.leo.viacep_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CepService {
+
+    public String searchCep(String cep) {
+        return cep;
+    }
+
+}
